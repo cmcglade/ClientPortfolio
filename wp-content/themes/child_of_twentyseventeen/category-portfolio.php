d784114cb4c7c34f195f22eeb1fddc1757c22007
cmcglade <cmcglade825@gmail.com>
2019-06-14 14:46:23 -0400

    changed multiple php templates.  created no title template, removed title from portfolio template, and removed wordpress copyright from footer template

--- wp-content/themes/child_of_twentyseventeen/category-portfolio.php
+++ wp-content/themes/child_of_twentyseventeen/category-portfolio.php
@@ -16,10 +16,11 @@ get_header(); ?>
 
 	<?php if ( have_posts() ) : ?>
 		<header class="page-header">
-			<?php
-				//the_archive_title( '<h1 class="page-title">', '</h1>' );
+			<h1 class="entry-title">Case Studies</h1>
+			<!-- <?php
+				the_archive_title( '<h1 class="page-title">', '</h1>' );
 				the_archive_description( '<div class="taxonomy-description">', '</div>' );
-			?>
+			?> --> <!--commenting out category title for portfolio page-->
 		</header><!-- .page-header -->
 	<?php endif; ?>
 
